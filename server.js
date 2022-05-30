051fb9f497235e8ab7a4631033a351102a539d24
PJScript <kilo718@naver.com>
2022-05-30 15:58:23 +0900

    fix origin

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors({
-    origin:'http://kkyoyangedu.com:8080',
+    origin:'http://kkyoyangedu.com',
     methods: ['GET', 'POST'],
     credentials: true,
 }))
