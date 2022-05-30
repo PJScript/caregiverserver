355d8847bddac676c1f10269e85687db57cef412
PJScript <kilo718@naver.com>
2022-05-30 17:27:49 +0900

    Test

--- server.js
+++ server.js
@@ -38,13 +38,13 @@ const s3 = new AWS.S3({
 const app = express();
 
 
-// app.use(express.json({
-//     limit: '10mb'
-//   }))
-  app.use(express.urlencoded({
-    limit: '10mb',
-    extended: false
+app.use(express.json({
+    limit: '10mb'
   }))
+//   app.use(express.urlencoded({
+//     limit: '10mb',
+//     extended: false
+//   }))
 
   app.use(cors({
     origin:'http://kkyoyangedu.com',
